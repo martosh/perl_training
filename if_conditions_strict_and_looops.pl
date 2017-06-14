1ef2d0988d5aa8e21ee927961e4590316f835916
mgrigorov <martin.grigorov@experian.com>
2017-06-14 16:59:26 +0300

    Session 4

--- if_conditions_strict_and_looops.pl
+++ if_conditions_strict_and_looops.pl
@@ -3,49 +3,53 @@ use 5.10.1;
 use Data::Dumper;
 use warnings;
 
-# some linux commands, grep, cat, ls, cd, man, locate, find, cut, tail, head, tar mv, mkdir
+# some well known linux commands, grep, cat, ls, cd, man, locate, find, cut, tail, head, tar mv, mkdir
 
-# use strict pragmatic statement
-#
+# use strict pragmatic statement force you to declare the perl data structure my @data, my %hash, my $test , my ($variable1, $variable2);
+
 my $name = 'Marto';
-my $test; 
-my @names = ( 'a'.. 'z');
+my $test;
+
+# defines array with letter as elements
+my @names = ('a' .. 'z');
 
 if ($test == 'asdasd' ) {
     $test = '123123';
 }
+
 say $name;
 
-#
+# if conditions, || && > < == eq != ne >= <=
+# If statement with more that one condition 
+
 if (defined($test) && $test == '123123' ) {
         say "The test variable is defined and also is equal to 123123 test[$test]";
 }
-#
-if ($test || $test == '123123' ) { #
+# If statment with two conditions with or
+if ($test || $test == '123123' ) { 
     say "$test";
 }
-#
+
+# one liner if, print me the statement if condition is true 
 say "This is my statement" if $test eq '123123';
 
+#ternare if else
 $test ? say "This is my statement" : say "not defined";
 
-# if conditions, || && > < == eq != ne >= <=
- 
-# loops for ( $i; $test == 10; $i++ );
-#
+# loops  
+# C style for loop looks like this, very uncommon for perl  for ( $i; $test == 10; $i++ );
 
-#foreach while
-for (@names) { # a .. z
+#foreach, we can use for as short, instead a foreach word 
+for (@names) { # a .. z # for on forcheach will be same here
    say $_;
    if ($_ eq 'v') {
         say "there is v in the array ";
-   } else {
-        say "This element[$_] in not v";
    }
 }
 
 my $counter;
 
+# While condition is true execute the block
 while (  $counter <= 10 ) { 
     $counter++;
     say $counter;
---
+++ read_write_file.pl
@@ -0,0 +1,93 @@
+use strict;
+use 5.10.1;
+use Data::Dumper;
+use File::Slurp;
+
+#perldoc sintaxis open FILEHANDLE,MODE,EXPR
+
+my $filename = 'data.txt';
+# content of data.txt
+# Gosho
+# Tosho
+# Joro
+
+#open(my $fh, '<', $filename) #works as well but it's good to be more specific with the encoding
+open(my $fh, '<:encoding(UTF-8)', $filename)
+    or die "Could not open file '$filename' [$!]";
+
+# remember that when we use strict we need to declare all data with my
+my @names; 
+
+# looping over filehandler line by line
+while (my $row = <$fh>) {
+    # row must be chomped as well like with <STDIN> input 
+    chomp $row;
+    # fill @names array with row 
+    push @names, $row;
+}
+
+#remember to close file handelers
+close $fh or die "Cannot close fh with file[$filename]"; 
+
+# Let us print the array filled with names
+say Dumper \@names;
+
+########################
+#Writing in file 
+########################
+
+# plese not >> and > are different cases '>>' is used to append
+open(my $wr_fh, '>>', 'numbers.txt')
+  or die "Could not open file 'numbers.txt' std_err[$!]";
+
+# define array with numbers from 1 until 100 
+my @numbers = ( 1 .. 100);
+
+#cycle them and print them in some file 
+for my $number ( @numbers) {
+    print $wr_fh "$number\n";
+}
+
+###########################################
+####### Other ways for reading file #######
+###########################################
+# use File::Slurp module 
+my @lines = read_file($filename);
+say Dumper \@lines;
+
+
+########################
+# LOOP LABELS
+########################
+
+# according to Damian Conway's book Perl best practice, we need to use Labels all the time
+LINE: foreach my $line (@lines ) {
+    chop($line);
+    say $line;
+    last;
+}
+
+#################### Basic Regular Expressions ##################
+my $name = 'Iwan    Ivanov ';
+# \s - space sign
+# \d - digit 
+# \w - word
+# .  - any character
+# *  - zero or more signs
+# +  - one or more signs
+# Examples:
+# \s* - zero or more spaces
+# \d+ - one or more digits
+# \w* - zero words or more
+# ()  - Caches what we desire
+# (\w+) - Caches one or more word chars
+my @names;
+# use regex in if to aboid bugs with caching variables $1;
+
+'asdasd' =~ /(asd)/;
+say "show me [$1]";
+
+$name =~ /(\d+)\s+(\w+)/;
+say "There is searched char[$1] second[$2] in name[$name]";
+push @names, $2;
+
